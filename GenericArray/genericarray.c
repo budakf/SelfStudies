4237aa99a8e0872bf2aa242004c61d92888e2485
Fatih <budakf@itu.edu.tr>
2019-06-09 00:21:10 +0300

    added dynamic feature on generic array

--- GenericArray/genericarray.c
+++ GenericArray/genericarray.c
@@ -13,21 +13,28 @@ void init_GenericArray(GenericArray * array, int capacity){
     array->container = malloc( sizeof(void *) * capacity );
 }
 
-// void enhance_capacity(GenericArray * array){
-//     GenericArray * temp = array;
-//     init_GenericArray(array, array->capacity*2);
-//     array->size = temp->size;
-//     array->nextIndex = temp->nextIndex;
-// }
+void copy_elements( GenericArray * dest, GenericArray * source ){
+    for(int i = 0; i < source->size; i++){
+        dest->container[i] = malloc( sizeof(void *) );
+        dest->container[i] = source->container[i];
+    }
+}
 
-// void copy_elements( GenericArray * dest, GenericArray * source ){
-//     for(int i = 0; i < source->size; i++){
-//         dest->container[i] = malloc( sizeof(void *) );
-//     }
-// }
-
+void enhance_capacity(GenericArray * array){
+    GenericArray * temp = malloc( sizeof(GenericArray) );
+    temp->container = malloc( sizeof(void *) * array->size );
+    temp->size = array->size;
+    copy_elements(temp, array);
+    init_GenericArray(array, array->capacity*2);
+    copy_elements(array, temp);
+    array->size = temp->size;
+    free(temp);
+}
 
 void push_back( GenericArray * array , void * element){
+    if(array->size == array->capacity){
+        enhance_capacity(array);
+    }
     array->container[array->size] = malloc( sizeof(void *) );
     array->container[array->size] = element;
     ++array->size;
@@ -41,17 +48,21 @@ void pop_back(GenericArray * array){
 
 int main(){
 
-    GenericArray *array;
-    init_GenericArray(array , 10);
+    GenericArray *array = malloc( sizeof(GenericArray) );
+    init_GenericArray(array , 1);
 
     int first = 10;
     char second = 'F';
     push_back(array, &first );
+    push_back(array, &first );
+    push_back(array, &first );
+    push_back(array, &first );
+    push_back(array, &first );
     printf( "%d \n", *((int *)array->container[0]) );
-    //pop_back(array);
+    pop_back(array);
     push_back(array, &second );
-    printf( "%c\n", *((char *)array->container[1]) );
-    printf( "%d\n", array->size );
+    printf( "%c\n", *((char *)array->container[4]) );
+    printf( "%d\n%d\n", array->size, array->capacity );
 
     return 0;
 }
