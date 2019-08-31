ff98fe274d6fbde487cd9e7623bd1ac48e0e09bf
Fatih <budakf@itu.edu.tr>
2019-09-01 01:37:03 +0300

    added custom generic iterator class

---
+++ MySTL/myiterator.cpp
@@ -0,0 +1,63 @@
+#ifndef MYITERATOR_CPP
+#define MYITERATOR_CPP
+
+template <typename T>
+class MyIterator{
+
+public:
+    MyIterator(T * _ptr = nullptr) : mPtr(_ptr){ }
+
+    MyIterator(const MyIterator<T> & iterator): mPtr(iterator.mPtr){}
+
+    ~MyIterator(){
+        //delete mPtr;  it is not neccessary because iterator is not owner of the object. Only owner must destruct object.
+        //              iterator is assistant class to iterate container
+    }
+
+    MyIterator<T> & operator++(){
+        mPtr++;
+        return *this;
+    }
+
+    MyIterator<T> operator++(int){ // Postfix
+        MyIterator<T> temp = *this;
+        mPtr++;
+        return temp;
+    }
+
+    MyIterator<T> & operator--(){
+        mPtr--;
+        return *this;
+    }
+
+    MyIterator<T> operator--(int){ // Postfix
+        MyIterator<T> temp = *this;
+        mPtr--;
+        return temp;
+    }
+
+    bool operator==(MyIterator<T> iterator){
+        return mPtr == iterator.mPtr;
+    }
+
+    bool operator!=(MyIterator<T> iterator){
+        return mPtr != iterator.mPtr;
+    }
+
+    T & operator*(){
+        return *mPtr;
+    }
+
+    void advance(unsigned int number){
+        mPtr+=number;
+    }
+
+
+
+private:
+    T * mPtr;
+
+
+};
+
+#endif
