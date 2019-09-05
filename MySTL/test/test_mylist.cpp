fe8e3c7b737818fd796158d9f00792879564b3ab
Fatih <budakf@itu.edu.tr>
2019-09-06 01:06:50 +0300

    added test function for insert operation

--- MySTL/test/test_mylist.cpp
+++ MySTL/test/test_mylist.cpp
@@ -57,6 +57,42 @@ private slots:
         QCOMPARE(*(intList.front()->next->mData), 726);
     }
 
+    void test_insert(){
+        int * data = new int{98};
+        Node<int> *node = new Node<int>{data};
+        intList.pushBack(node);
+        QCOMPARE(intList.length(), 3);
+        QCOMPARE(*(intList.front()->mData), 76);
+
+        int * data2 = new int{54};
+        Node<int> *node2 = new Node<int>{data2};
+        intList.pushBack(node2);
+        QCOMPARE(intList.length(), 4);
+        QCOMPARE(*(intList.front()->next->mData), 726);
+
+        int * data3 = new int{23};
+        Node<int> *node3 = new Node<int>{data3};
+        intList.pushBack(node3);
+        QCOMPARE(intList.length(), 5);
+
+        int * data4 = new int{55};
+        Node<int> *node4 = new Node<int>{data4};
+        intList.pushBack(node4);
+        QCOMPARE(intList.length(), 6);
+
+        int * data5 = new int{87};
+        Node<int> *node5 = new Node<int>{data5};
+        intList.insert(node5, 2);
+        QCOMPARE(intList.length(), 7);
+        QCOMPARE(*(intList.front()->next->next->mData), 87);
+
+        int * data6 = new int{234};
+        Node<int> *node6 = new Node<int>{data6};
+        intList.insert(node6, 0);
+        QCOMPARE(intList.length(), 8);
+        QCOMPARE(*(intList.front()->mData), 234);
+    }
+
     void test_begin_end(){
         int * data = new int{76};
         Node<int> *node = new Node<int>{data};
